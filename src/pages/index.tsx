a8031e9a637b6af7b5d4b8c45c08bf9493fc29c8
Ollie Beenham <ollie@beenhamow.co.uk>
2024-03-28 14:16:42 +0000

    Removed placeholder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
 import MasterPage from "@/components/Layout/maxter";
 
 export default function Home() {
-    return (
-        <MasterPage pageTitle="Home">
-            <p>Jack Smells</p>
-        </MasterPage>
-    );
+    return <MasterPage pageTitle="Home"></MasterPage>;
 }
