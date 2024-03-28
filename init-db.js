e3daf768b4fd3011aae381b8c81f2c8dd1e1632e
Ollie Beenham <ollie@beenhamow.co.uk>
2024-03-28 18:15:51 +0000

    Added the creation of a default user with a hashed password

--- init-db.js
+++ init-db.js
@@ -1,7 +1,14 @@
+/**
+ * @author - @ElBeenMachine
+ */
+
+// Import the required modules
 const mysql = require("mysql2/promise");
+const bcrypt = require("bcrypt");
 
 /**
  * Function to establish a connection to the MySQL database.
+ *
  * @param {number} attempts - Number of connection attempts made so far.
  * @returns {Promise} - A Promise that resolves with the database connection.
  */
@@ -21,36 +28,37 @@ async function connectToDb(attempts = 0) {
             database,
         });
 
-        console.log("Connected to the database!");
+        console.log("🟢 | Connected to the database");
         return connection;
     } catch (err) {
-        console.error(err.message || err.stack);
-        console.error("Unable to connect to the database. Retrying in 5 seconds...");
+        console.warn("🟠 | Unable to connect to the database. Retrying in 5 seconds...");
         // Retry connection up to 5 times
         if (attempts < 10) {
             await new Promise((resolve) => setTimeout(resolve, 5000));
             return connectToDb(attempts + 1);
         } else {
-            throw new Error("Unable to connect to the database after 10 attempts.");
+            throw new Error("🔴 | Unable to connect to the database after 10 attempts.");
         }
     }
 }
 
-// Attempt to establish a connection to the database and perform operations
-async function main() {
-    let connection;
-    try {
-        connection = await connectToDb();
-        // Check if the 'uptimeUsers' table already exists
-        const [rows] = await connection.execute("SHOW TABLES LIKE 'uptimeUsers'");
-        if (rows.length > 0) {
-            console.log("uptimeUsers table already exists.");
-            return;
-        }
+/**
+ * Function to check if there is already a users table in the database. If the table does not exist, it will be created.
+ *
+ * @param {mysql.Connection} connection
+ * @returns {boolean} Returns true if the table already exists, false otherwise.
+ */
+async function checkUserTable(connection) {
+    // Check if the 'uptimeUsers' table already exists
+    const [rows] = await connection.execute("SHOW TABLES LIKE 'users'");
+    if (rows.length > 0) {
+        console.warn("🟠 | Users table already exists.");
+        return true;
+    }
 
-        // Define the SQL query to create the 'uptimeUsers' table
-        const createTableQuery = `
-            CREATE TABLE IF NOT EXISTS uptimeUsers (
+    // Create the users table if it did not exist
+    const createTableQuery = `
+            CREATE TABLE IF NOT EXISTS users (
                 userID CHAR(36) PRIMARY KEY DEFAULT (UUID()),
                 firstName VARCHAR(255),
                 lastName VARCHAR(255),
@@ -59,30 +67,75 @@ async function main() {
                 isAdmin BOOLEAN
             )`;
 
+    try {
         // Execute the SQL query to create the 'uptimeUsers' table
         await connection.execute(createTableQuery);
-        console.log("uptimeUsers table created successfully.");
+        console.log("🟢 | Users table successfully created");
+    } catch (error) {
+        console.error("🔴| Error creating users table: ", error);
+    } finally {
+        return false;
+    }
+}
 
-        // Define the SQL query to create a default user
-        const createDefaultUserQuery = `
-            INSERT INTO uptimeUsers (firstName, lastName, password, email, isAdmin)
-            VALUES ('Admin', 'User', 'admin', 'admin@example.com', true)`;
+/**
+ * Function to hash a plaintext password using bcrypt.
+ *
+ * @param {String} password The plaintet password to be hashed
+ * @returns {String} The hashed password
+ */
+async function hashPassword(password) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+}
 
+/**
+ * Function to check if there is already a default user in the database.
+ * If there is no users table, then a deafult user will be created.
+ * Otherwise, it will be assumed that one is already present.
+ *
+ * @param {mysql.Connection} connection
+ * @returns
+ */
+async function checkDefaultUser(connection) {
+    // Check if user table already exists
+    const tableExists = await checkUserTable(connection);
+
+    // If the table exists, return true
+    if (tableExists) return true;
+
+    // Create a hashed password
+    const hashedPassword = await hashPassword("changeMe");
+
+    // If the table did not exist, create a default user
+    const createDefaultUserQuery = `INSERT INTO users (firstName, lastName, password, email, isAdmin) VALUES ('Admin', 'User', '${hashedPassword}', 'admin@example.com', true)`;
+
+    try {
         // Execute the SQL query to create the default user
         await connection.execute(createDefaultUserQuery);
-        console.log("Default user created successfully.");
+        console.log("🟢 | Default user created successfully.");
     } catch (error) {
-        console.error(error);
-    } finally {
-        if (connection) {
-            // Close the database connection
-            try {
-                await connection.end();
-                console.log("MySQL connection closed.");
-            } catch (err) {
-                console.error("Error closing MySQL connection:", err.stack);
-            }
-        }
+        console.error("🔴 | Error creating default user: ", error);
+    }
+}
+
+/**
+ * Main function that establishes a connection to the database and performs the required operations to initialize it.
+ */
+async function main() {
+    // Create a connection to the database
+    const connection = await connectToDb();
+
+    // Check if the default user exists
+    const user = await checkDefaultUser(connection);
+
+    // Close the database connection
+    try {
+        await connection.end();
+        console.log("🟢 | MySQL connection closed.");
+    } catch (error) {
+        console.error("🔴 | Error closing MySQL connection: ", error);
     }
 }
 
