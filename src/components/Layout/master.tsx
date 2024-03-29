92d283ace8c23cbe9fc62acfec374326eddab268
Ollie Beenham <ollie@beenhamow.co.uk>
2024-03-29 16:55:53 +0000

    Updated login page styling

--- src/components/Layout/master.tsx
+++ src/components/Layout/master.tsx
@@ -1,6 +1,15 @@
 import Head from "next/head";
 import React from "react";
 
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-poppins",
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
 interface MasterPageProps {
     children?: React.ReactNode;
     pageTitle: string;
@@ -8,7 +17,7 @@ interface MasterPageProps {
 
 function MasterPage({ children, pageTitle }: MasterPageProps) {
     return (
-        <main>
+        <main className={`${poppins.variable}`}>
             <Head>
                 <title>{pageTitle}</title>
             </Head>
