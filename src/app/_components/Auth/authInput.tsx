1dcded7a0d453e8dd99b6583c15f331486a2ca5c
Ollie Beenham <ollie@beenhamow.co.uk>
2024-06-02 19:27:51 +0100

    Removed autocomplete from login apge

--- src/app/_components/Auth/authInput.tsx
+++ src/app/_components/Auth/authInput.tsx
@@ -12,7 +12,17 @@ interface AuthInputProps {
 }
 
 const AuthInput = React.forwardRef<HTMLInputElement, AuthInputProps>(({ type, placeholder, required, name }, ref) => {
-    return <input ref={ref || null} className="border-solid border rounded-md px-4 py-2 w-full mb-2 text-black" type={type} placeholder={placeholder} name={name} required={required} />;
+    return (
+        <input
+            ref={ref || null}
+            className="border-solid border rounded-md px-4 py-2 w-full mb-2 text-black"
+            type={type}
+            placeholder={placeholder}
+            name={name}
+            required={required}
+            autoComplete="off"
+        />
+    );
 });
 
 export default AuthInput;
